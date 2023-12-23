a6579e1be5d0494a9d1eeabe6cf051e85cd764a4
mgheffel <mgheffel@gmail.com>
2023-12-22 17:59:55 -0800

    Update CondaCommands.sh

--- CondaCommands.sh
+++ CondaCommands.sh
@@ -1,2 +1,5 @@
 # create a new environment with a name and base python version
 conda create -n myenv python=3.8
+
+#checking if this works and is sufficent
+conda create --name scanpy3_env -c conda-forge -c bioconda scanpy seaborn leidenalg fa2
