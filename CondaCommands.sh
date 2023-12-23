77029c69f82353477e49f425460691acfb695d43
mgheffel <mgheffel@gmail.com>
2023-12-22 18:33:20 -0800

    Update CondaCommands.sh
    forgot nb_conda_kernels

--- CondaCommands.sh
+++ CondaCommands.sh
@@ -2,4 +2,4 @@
 conda create -n myenv python=3.8
 
 #checking if this works and is sufficent
-conda create --name scanpy3_env -c conda-forge -c bioconda scanpy seaborn leidenalg fa2
+conda create --name scanpy3_env -c conda-forge -c bioconda scanpy seaborn leidenalg fa2 nb_conda_kernels ipykernel
